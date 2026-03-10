6cff52e2f0cff7f85fd22cab2be52999c5ef8afe
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-03-10 14:29:51 +0100

    Complete lesson 14: Modal Animation (part 2)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable */
 import { motion, AnimatePresence } from 'motion/react';
+import { Link } from 'react-router';
 
 const backdropVariant = {
   hidden: { opacity: 0 },
   visible: { opacity: 1 },
+};
+
+const modalVariant = {
+  hidden: { y: '-100%', opacity: 0 },
+  visible: { y: '100%', opacity: 1, transition: { delay: 0.2 } },
 };
 
 function Modal({ showModal, setShowModal }) {
@@ -15,7 +21,15 @@ function Modal({ showModal, setShowModal }) {
           variants={backdropVariant}
           initial='hidden'
           animate='visible'
-        ></motion.div>
+          exit='hidden'
+        >
+          <motion.div className='modal' variants={modalVariant}>
+            <p>Want to make another pizza?</p>
+            <Link to='/'>
+              <button onClick={() => setShowModal(false)}>Start Again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
       )}
     </AnimatePresence>
   );
